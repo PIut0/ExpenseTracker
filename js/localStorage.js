3254efe6304c4895ed6e23d7437fbdaac167bfe8
Plut0 <slimdoks@gmail.com>
2021-06-03 04:20:58 +0900

    feat: localStorage method

---
+++ js/localStorage.js
@@ -0,0 +1,29 @@
+const localStorage = window.localStorage;
+
+function initLocalStorageData() {
+	let ret = {
+		"incomeCount": 0,
+		"expenseCount": 0,
+		"balance": 0,
+		"expenseList": []
+	}
+
+	return (ret);
+}
+
+function getLocalStorageData() {
+	let ret = localStorage.getItem('expenseData');
+
+	if (!ret)
+		ret = initLocalStorageData();
+	else
+		ret = JSON.parse(ret);
+
+	return (ret);
+}
+
+function setLocalStorageData(data) {
+	localStorage.setItem('expenseData', JSON.stringify(data));
+}
+
+export { getLocalStorageData, setLocalStorageData };
